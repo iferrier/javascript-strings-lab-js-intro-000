e5f1817def5d4f281f6aac3be2c58ec2424b84d3
Isabelle Ferrier <ferrierisabelle@gmail.com>
2019-04-13 21:27:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,4 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
-function conversation(topic) {
-"Let's talk about "+ topic;
-}
-
-conversation(topic)
+"Let's talk about ${topic}"
